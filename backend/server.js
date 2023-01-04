4792ac524963249bb8af7030196857df77dc5f87
Mateusz <wosmateusz2@gmail.comm>
2023-01-04 18:36:41 +0100

    Create backend controller and route for adding review

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
@@ -16,6 +17,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+if(process.env.NODE_ENV ==='development'){
+	app.use(morgan('dev'))
+}
 
 app.get('/', (req, res) => {
 	res.send('API is running');
